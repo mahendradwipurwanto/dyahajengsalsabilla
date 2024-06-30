75f40cfa14a4896b7a466cf0d8dd7443d46a37f9
mahendradwipurwanto <mahendradwipurwanto@gmail.com>
2024-06-30 22:43:33 +0700

    first commit

---
+++ src/app/pages/dashboards/dashboard/dashboard.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core'
+
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+
+/**
+ * Ecommerce Component
+ */
+export class DashboardComponent implements OnInit {
+ // bread crumb items
+ breadCrumbItems!: Array<{}>;
+
+
+
+ ngOnInit(): void {
+   /**
+    * BreadCrumb
+    */
+   this.breadCrumbItems = [
+     { label: 'Dashboards' },
+     { label: 'Dashboard', active: true }
+   ];
+ }
+}
